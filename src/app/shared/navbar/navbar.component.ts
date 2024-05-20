ef9cbe2cd6a3ebbda0bedef47bc39b6693430cf1
coque <mateogabus@gmail.com>
2024-05-20 18:29:12 -0300

    Added design elements, animations, and contact form

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -9,4 +9,32 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isHidden = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    if (window.scrollY > 50) {
+      this.isHidden = true;
+      navbar.classList.add('hidden-navbar');
+    } else {
+      this.isHidden = false;
+      navbar.classList.remove('hidden-navbar');
+    }
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    const navbar = document.querySelector('.navbar') as HTMLElement;
+    navbar.classList.remove('hidden-navbar');
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    if (window.scrollY > 50) {
+      const navbar = document.querySelector('.navbar') as HTMLElement;
+      navbar.classList.add('hidden-navbar');
+    }
+  }
+}
