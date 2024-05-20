4d241d660680c5263f718ab4c174fb773e0775b6
coque <mateogabus@gmail.com>
2024-05-20 17:16:10 -0300

    Configured Firebase and adjusted providers

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAcAwsdJxMMcWre1DboyDJN0v58S_AakNs',
+    authDomain: 'pegasuz-9c3c5.firebaseapp.com',
+    projectId: 'pegasuz-9c3c5',
+    storageBucket: 'pegasuz-9c3c5.appspot.com',
+    messagingSenderId: '853599363065',
+    appId: '1:853599363065:web:1c0a1bac8d5e61699fa728',
+    measurementId: 'G-2QYCW10BGF',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './app/environment/environment';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err));
