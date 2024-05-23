18748e4e28a8e4fa443cc4050f080694b3731014
coque <mateogabus@gmail.com>
2024-05-23 09:33:46 -0300

    sidebar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,55 +1,26 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule,
-  ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-  isHidden = false;
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    const navbar = document.querySelector('.navbar') as HTMLElement;
-    if (window.scrollY > 50) {
-      this.isHidden = true;
-      navbar.classList.add('hidden-navbar');
+  toggleSidebar(): void {
+    const sidebar = document.querySelector('.sidebar') as HTMLElement;
+    const toggleBtn = document.querySelector('.toggle-btn i') as HTMLElement;
+    sidebar.classList.toggle('open');
+    sidebar.classList.toggle('collapsed');
+    if (sidebar.classList.contains('open')) {
+      toggleBtn.classList.remove('fa-chevron-right');
+      toggleBtn.classList.add('fa-chevron-left');
     } else {
-      this.isHidden = false;
-      navbar.classList.remove('hidden-navbar');
-    }
-  }
-
-  @HostListener('mouseenter')
-  onMouseEnter() {
-    const navbar = document.querySelector('.navbar') as HTMLElement;
-    navbar.classList.remove('hidden-navbar');
-  }
-
-  @HostListener('mouseleave')
-  onMouseLeave() {
-    if (window.scrollY > 50) {
-      const navbar = document.querySelector('.navbar') as HTMLElement;
-      navbar.classList.add('hidden-navbar');
+      toggleBtn.classList.remove('fa-chevron-left');
+      toggleBtn.classList.add('fa-chevron-right');
     }
   }
 }
